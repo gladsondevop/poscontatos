0da098ac730549c4d7c3b83161511aeada42be99
Gladson Cunha de Albuquerque - 5462 <gladsonc@sescdf.com.br>
2024-04-12 00:09:13 -0300

    Inclusão das configurações do Promotheus.

--- src/main/java/com/pos/contatos/controller/ContatoController.java
+++ src/main/java/com/pos/contatos/controller/ContatoController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.pos.contatos.model.Contato;
 import com.pos.contatos.service.ContatoService;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @Controller
 @RequestMapping("contatos")
 public class ContatoController {
@@ -19,8 +22,17 @@ public class ContatoController {
 	@Autowired
 	private ContatoService service;
 	
+	private final MeterRegistry meterRegistry;
+	
+	public ContatoController(MeterRegistry meterRegistry) {
+		this.meterRegistry = meterRegistry;
+	}
+
 	@GetMapping
 	public ResponseEntity<List<Contato>> listarContatos() {
+		Counter counter = Counter.builder("quantidade_acessos").tag("acessos_contatos", "getAll")
+				.description("Quantidades de acessos para consultar todos os contatos").register(meterRegistry);
+		counter.increment();
 		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(service.listarContatos());
 	}
 
